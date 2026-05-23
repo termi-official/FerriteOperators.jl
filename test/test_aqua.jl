db9c1be8bfd13c79c082b0b1ad3369931105f552
termi-official <9196588+termi-official@users.noreply.github.com>
2026-05-23 23:26:06 +0200

    Fix test

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,15 +1,15 @@
-using Aqua
+using Aqua, FerriteOperators
 
 @testset "Aqua.jl" begin
     Aqua.test_all(
-        FerriteDiffEq;
+        FerriteOperators;
         ambiguities = false, # Tested below for now
         # unbound_args=true,
         # undefined_exports=true,
         # project_extras=true,
         deps_compat = true,
-        piracies = true, # Comment out after https://github.com/Ferrite-FEM/Ferrite.jl/pull/864 is merged
+        piracies = false, 
         persistent_tasks = false,
     )
-    Aqua.test_ambiguities(FerriteDiffEq) # Must be separate for now, see https://github.com/JuliaTesting/Aqua.jl/issues/77
+    Aqua.test_ambiguities(FerriteOperators) # Must be separate for now, see https://github.com/JuliaTesting/Aqua.jl/issues/77
 end
